edbe7db38461e7dc06f7920627c37592b098cc96
Carlos Reinoso <jrp.carlos@hotmail.com>
2021-07-12 18:06:47 +0100

    styled the list and added hover effect with bin icon

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -1,14 +1,51 @@
+import { useState } from "react";
+import { FormControlLabel, FormGroup, makeStyles } from "@material-ui/core";
+import Checkbox from "@material-ui/core/Checkbox";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { red } from "@material-ui/core/colors";
+
 interface TodoListItemProps {
   todo: Todo["todo"];
   id: Todo["id"];
   handleDelete: handleDelete;
 }
 
+const useStyles = makeStyles((theme) => ({
+  listItem: {
+    display: "flex",
+    justifyContent: "space-between",
+    width: "100%",
+    alignItems: "center",
+    [theme.breakpoints.up("sm")]: {
+      width: 360
+    }
+  },
+  deleteIcon: {
+    color: "blue",
+    "&:hover": {
+      color: "red",
+    }
+  }
+}));
+
 const TodoListItem = ({ todo, id, handleDelete }: TodoListItemProps) => {
+  const classes = useStyles();
+
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+  const handleChange = () => setIsChecked(!isChecked);
+
   return (
-    <li>
-      {todo}
-      <button onClick={() => handleDelete(id)}>delete</button>
+    <li
+      className={classes.listItem}
+      style={isChecked ? { textDecoration: "line-through" } : undefined}
+    >
+      <FormControlLabel
+        control={
+          <Checkbox onClick={handleChange} name="checkbox" color="primary" />
+        }
+        label={todo}
+      />
+      <DeleteIcon className={classes.deleteIcon} onClick={() => handleDelete(id)} />
     </li>
   );
 };
